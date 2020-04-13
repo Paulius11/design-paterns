093eeafd9e578314e63a9eee7ca94a9774835381
Paulius11 <paulius.gaizauskas@gmail.com>
2020-04-13 10:14:41 +0300

    Update SinglTest.java

--- singleton/SinglTest.java
+++ singleton/SinglTest.java
@@ -2,7 +2,6 @@ public class SinglTest {
 
     public static void main(String[] args) {
         Singleton singleObject = Singleton.getInstance();
-        Singleton.demoMethod();
         Singleton tmp2 = Singleton.getInstance();
         System.out.println(singleObject == tmp2);
 
