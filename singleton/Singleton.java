f126ddf3cbbca1176d25a197c1876d235b5d38cd
Paulius11 <paulius.gaizauskas@gmail.com>
2020-04-13 10:14:23 +0300

    Update Singleton.java

--- singleton/Singleton.java
+++ singleton/Singleton.java
@@ -1,20 +1,13 @@
 public class Singleton {
 
-    private static Singleton singleton = new Singleton();
+    private static Singleton singleton;
+    
+    private Singleton() {}
 
-    /*
-     * A private Constructor prevents any other class from instantiating.
-     */
-    private Singleton() {
-    }
-
-    /* Static 'instance' method */
     public static Singleton getInstance() {
+        if(singleton == null)
+            singleton = new Singleton();
         return singleton;
     }
-
-    /* Other methods protected by singleton-ness */
-    protected static void demoMethod() {
-        System.out.println("demoMethod for singleton");
-    }
+    
 }
